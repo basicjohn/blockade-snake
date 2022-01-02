182e16fc2dbe53565633c0bc395782d934694bba
basicjohn <edmondsonj@gmail.com>
2022-01-02 13:05:15 -0800

    create board units

---
+++ snake.js
@@ -0,0 +1,21 @@
+
+let playArea = document.getElementById("game");
+let rows = 23;
+let columns = 35;
+
+function createPlayArea() {
+
+}
+
+// drawSnake([
+//   [0, 0],
+//   [1, 0],
+//   [2, 0],
+//   [3, 0],
+//   [4, 0]
+// ]
+// )
+
+// function drawSnake() {
+
+// }
